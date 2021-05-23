b09ec2a200e7f79e7ee3bdd13753ca1306e68efc
Hideo Hattori <hhatto.jp@gmail.com>
2021-05-23 15:38:45 +0900

    response png

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,24 @@
 import { ServerRequest } from "https://deno.land/std@0.97.0/http/server.ts";
+import { Image } from "https://deno.land/x/imagescript@1.2.6/mod.ts";
+
+const __filename = new URL(".", import.meta.url).pathname;
+const RESOURCE_ROOT = `${__filename}`;
 
 export default async (req: ServerRequest) => {
-	req.respond({ body: `Hello, from Deno v${Deno.version.deno}!` });
+  const binary = await Deno.readFile(`${RESOURCE_ROOT}/base.png`);
+  const font = await Deno.readFile(`${RESOURCE_ROOT}/Roboto-Regular.ttf`);
+  const image = await Image.decode(binary);
+  const helloText = Image.renderText(font, 120, "hello", 0xffffffff);
+  image.composite(helloText, 30, 100);
+
+  const encoded = await image.encode(1);
+
+  const headers = new Headers();
+  headers.set('Content-Type', 'image/png');
+
+  req.respond({
+    status: 200,
+    headers,
+    body: encoded,
+  });
 };
